6f6a560858cadb51133224fc2cce263a37ffc72a
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-04-22 21:00:02 +0200

    Creado MainDeMentira y distinto método (todo en ramaEX)

--- HolaMundoRamas/src/main/java/com/salesianostriana/dam/holamundoramas/MainDeMentira.java
+++ HolaMundoRamas/src/main/java/com/salesianostriana/dam/holamundoramas/MainDeMentira.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MainDeMentira {
 	
-	public void gritar() {
-		System.out.println("Haz los deberes de entornos de desarrollo y aprende a usar las ramas.");
+	public void regañar() {
+		System.out.println("¡¡¡HAZ LOS DEBERES DE ENTORNOS ZOPENCO!!!");
 	}
 }
