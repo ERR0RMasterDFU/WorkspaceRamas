10efbd922eb6d8888fe7e54a39c21b68888edc89
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-04-22 20:52:25 +0200

    Creado holaMundoRamas y clase MainDeMentira

---
+++ HolaMundoRamas/src/main/java/com/salesianostriana/dam/holamundoramas/MainDeMentira.java
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.holamundoramas;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainDeMentira {
+	
+	public void gritar() {
+		System.out.println("Haz los deberes de entornos de desarrollo y aprende a usar las ramas.");
+	}
+}
